acad87102c20f71c07f4153bbc750aa75ad6f1f1
Tomasz Sęk <tomasz.sek@naveo.pl>
2022-02-17 15:24:32 +0100

    aster

--- app.py
+++ app.py
@@ -4,37 +4,24 @@ import csv
 
 app = Flask(__name__)
 
-def get_rates():
-    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-    data = response.json()
-    data = data[0]['rates']
-
-    with open('rates.csv', 'w', encoding='UTF-8', newline='') as file:
-        fieldnames = ['currency', 'code', 'bid', 'ask']
-        writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter=';')
-        writer.writeheader()
-        writer.writerows(data)
-get_rates()
-
 @app.route('/', methods=['GET', 'POST'])
+
 def home():
+
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json") 
+    dane = response.json()
+
     if request.method == 'GET':
-        response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json") 
-        dane = response.json()
         stawki = dane[0]['rates']
         kody_walut = [d['code'] for d in stawki]
         print(kody_walut)
-        render_template('index.html', data=kody_walut)
-    else:
-        return render_template('index.html')
-
+        return render_template('index.html', data=kody_walut)
+    
     if request.method == 'POST':
+        print(dane)
         try:          
-            response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")  
-            data = response.json()
-
-            rates = data[0]['rates']
-            tradingDay = data[0]['tradingDate']
+            rates = dane[0]['rates']
+            tradingDay = dane[0]['tradingDate']
             
             currency_name = rates[0]['currency']
             code = rates[0]['code']
@@ -53,16 +40,12 @@ def home():
                     bid = currency['bid']
                     from_c = currency['code']
 
-            #zobaczyć co jest wykorzystane w szablonie 
             return render_template('index.html', amount=amount, currency_name=currency_name, response=response, 
                                    currency=currency, from_c=from_c, tradingDay=tradingDay, code=code, rates=rates,
                                    bid=bid, result=round(result, 2))
         
         except Exception as e:
             return '<h1>Bad Request : {}</h1>'.format(e)
-  
-    else:
-        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
